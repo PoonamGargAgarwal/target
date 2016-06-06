d2838e4d44bc83e7b4e672b8201b186031101e3a
PoonamGargAgarwal <poonamgargagrwl@gmail.com>
2016-06-06 16:55:12 -0500

    Update ProductController.java

--- src/main/java/org/myretail/controller/ProductController.java
+++ src/main/java/org/myretail/controller/ProductController.java
@@ -2,6 +2,7 @@ package org.myretail.controller;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.myretail.dto.ResponseDTO;
 import org.myretail.entities.Product;
 import org.myretail.entities.ProductPrice;
@@ -17,22 +18,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 @RequestMapping("/product")
 public class ProductController {
+	private static final Logger logger = Logger.getLogger(ProductController.class);
 	@Autowired
 	public  IProductService productService;
-	@RequestMapping(value = "/getall", method = RequestMethod.GET,produces = "application/json")
-	public @ResponseBody ResponseDTO<Product> getall() {
-
-		List<Product> products = productService.readAll();
-		
-		if (products != null) {
-			return new ResponseDTO<Product>(true, products);
-		}
-
-		return new ResponseDTO<Product>(false);
-	}
 	@RequestMapping(value = "/{productId}",headers = {"Accept=application/json"}, method = RequestMethod.GET,produces = "application/json")
 	public @ResponseBody ResponseDTO<Product> getProduct(@PathVariable long productId) {
 
+		logger.info("Product Id is :" + productId);
 		Product products = productService.read(productId);
 		
 		if (products != null) {
@@ -43,16 +35,12 @@ public class ProductController {
 	}
 	@RequestMapping(value = "/{productId}",headers = {"Accept=application/json"}, method = RequestMethod.PUT,produces = "application/json")
 	public @ResponseBody ResponseDTO<Product> getProduct(@PathVariable long productId,ProductPrice productPriceView) {
-
-		System.out.println(productPriceView.getPrice());
-		System.out.println(productPriceView.getId());
+		logger.info("Product Id is :" + productId);
 		Product product1 = productService.read(productId);
-		Product products = productService.savePrice(productId,product1);		
-		
+		Product products = productService.savePrice(productId,product1);				
 		if (products != null) {
 			return new ResponseDTO<Product>(true, products);
-		}
-
+		} 
 		return new ResponseDTO<Product>(false);
 	}
 
