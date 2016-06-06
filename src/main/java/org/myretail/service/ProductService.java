f93ca0bb37d3a0e15932fd70748224d2af314890
PoonamGargAgarwal <poonamgargagrwl@gmail.com>
2016-06-06 16:57:05 -0500

    Update ProductService.java

--- src/main/java/org/myretail/service/ProductService.java
+++ src/main/java/org/myretail/service/ProductService.java
@@ -2,6 +2,8 @@ package org.myretail.service;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+import org.myretail.controller.ProductController;
 import org.myretail.entities.Product;
 import org.myretail.entities.ProductPrice;
 import org.myretail.repository.jpa.IProductRepository;
@@ -13,35 +15,32 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 @Transactional
 public class ProductService implements IProductService {
+
+	private static final Logger logger = Logger.getLogger(ProductService.class);
 	@Autowired
 	public IProductRepository productRepository;
-    @Autowired
-    public IProductPriceService productPriceService;
-	
+	@Autowired
+	public IProductPriceService productPriceService;
+
 	public Product read(Long id) {
-		Product  product = productRepository.findOne(id);
+		Product product = productRepository.findOne(id);
 		ProductPrice productPricelist = productPriceService.read(id.toString());
 		product.setPrice(productPricelist.getPrice());
 		return product;
 	}
-	@Override
-	public List<Product> readAll() {
-		// TODO Auto-generated method stub
-		//List<ProductPrice> productPricelist = productPricerepository.;
-		return productRepository.findAll();
-		
-	}
+
 	@Override
 	public Product savePrice(Long id, Product productView) {
-		// TODO Auto-generated method stub
-		Product  product = productRepository.findOne(id);		
-		if (product!=null) {
+
+		Product product = productRepository.findOne(id);
+		if (product != null) {
 			ProductPrice productPriceView = new ProductPrice();
 			productPriceView.setId(product.getpId().toString());
 			productPriceView.setPrice(productView.getPrice());
-			ProductPrice productPrice = productPriceService.save(productPriceView);
+			ProductPrice productPrice = productPriceService
+					.save(productPriceView);
 			product.setPrice(productPrice.getPrice());
-		} 
+		}
 		return product;
 	}
 
